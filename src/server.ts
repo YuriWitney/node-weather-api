920183e7613a87138d4a63ea75031a6ee8030a7f
Yuri Witney <scot39@gmail.com>
2021-11-01 12:19:12 -0300

    chore: style fix

--- src/server.ts
+++ src/server.ts
@@ -5,25 +5,25 @@ import { forecastController } from './controllers/forecast';
 import { Application } from 'express';
 
 export class SetupServer extends Server {
-	constructor(private port = 3000) {
-		super();
-	}
+  constructor(private port = 3000) {
+    super();
+  }
 
-	public init(): void {
-		this.setupExpress();
-		this.setupControllers();
-	}
+  public init(): void {
+    this.setupExpress();
+    this.setupControllers();
+  }
 
-	public getApp(): Application {
-		return this.app
-	}
+  public getApp(): Application {
+    return this.app;
+  }
 
-	private setupExpress(): void {
-		this.app.use(bodyParser.json());
-	}
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+  }
 
-	private setupControllers(): void {
-		const forecastControllers = new forecastController();
-		this.addControllers([forecastControllers])
-	}
+  private setupControllers(): void {
+    const forecastControllers = new forecastController();
+    this.addControllers([forecastControllers]);
+  }
 }
