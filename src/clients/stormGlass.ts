e532c1e459c4cb3cf180fa8fce628135cdeea774
Yuri Witney <scot39@gmail.com>
2021-11-02 15:26:57 -0300

    feat:response interpretation

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,5 +1,24 @@
 import { AxiosStatic } from 'axios';
 
+export interface StormGlassSource {
+  [key: string]: number;
+}
+
+export interface StormGlassPoint {
+  readonly time: string;
+  readonly swellDirection: StormGlassSource;
+  readonly swellHeight: StormGlassSource;
+  readonly swellPeriod: StormGlassSource;
+  readonly waveDirection: StormGlassSource;
+  readonly wavelHeight: StormGlassSource;
+  readonly windDirection: StormGlassSource;
+  readonly windSpeed: StormGlassSource;
+}
+
+export interface StormGlassForecastResponse {
+  hours: StormGlassPoint[];
+}
+
 export class StormGlass {
   readonly stormGlassAPIParams =
     'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
