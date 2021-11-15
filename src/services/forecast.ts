4a31085acc3829ade0542317b68de18edbe92379
Yuri Witney <scot39@gmail.com>
2021-11-15 09:56:29 -0300

    feat: forecast service added

---
+++ src/services/forecast.ts
@@ -0,0 +1,43 @@
+import { ForecastPoint, StormGlass } from '@src/clients/stormGlass';
+
+export enum BeachPosition {
+  S = 'S',
+  E = 'E',
+  W = 'W',
+  N = 'N',
+}
+
+export interface Beach {
+  lat: number;
+  lng: number;
+  name: string;
+  position: BeachPosition;
+  user: string;
+}
+
+export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
+
+export class Forecast {
+  constructor(protected stormGlass = new StormGlass()) {}
+
+  public async processForecastForBeaches(
+    beaches: Beach[]
+  ): Promise<BeachForecast[]> {
+    const pointsWithCorrectSource: BeachForecast[] = [];
+    for (const beach of beaches) {
+      const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
+      const enrichedBeachData = points.map((e) => ({
+        ...{
+          lat: beach.lat,
+          lng: beach.lng,
+          name: beach.name,
+          position: beach.position,
+          rating: 1,
+        },
+        ...e,
+      }));
+      pointsWithCorrectSource.push(...enrichedBeachData);
+    }
+    return pointsWithCorrectSource;
+  }
+}
