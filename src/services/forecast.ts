b99819a1edefcdb15b5fb54feb9a9d6d4b1dcda7
Yuri Witney <scot39@gmail.com>
2021-11-17 21:23:20 -0300

    feat: forecast service data completed

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -17,12 +17,16 @@ export interface Beach {
 
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
 
+export interface TimeForecast {
+	time: string,
+	forecast: BeachForecast[]
+}
 export class Forecast {
   constructor(protected stormGlass = new StormGlass()) {}
 
   public async processForecastForBeaches(
     beaches: Beach[]
-  ): Promise<BeachForecast[]> {
+  ): Promise<TimeForecast[]> {
     const pointsWithCorrectSource: BeachForecast[] = [];
     for (const beach of beaches) {
       const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
@@ -38,6 +42,23 @@ export class Forecast {
       }));
       pointsWithCorrectSource.push(...enrichedBeachData);
     }
-    return pointsWithCorrectSource;
+    return this.mapForecastByTime(pointsWithCorrectSource);
   }
+
+	private mapForecastByTime(forecast: BeachForecast[]): TimeForecast[]{
+		const forecastByTime: TimeForecast[] = [];
+		for (const point of forecast) {
+			const timePoint = forecastByTime.find((f) => f.time === point.time)
+			if(timePoint) {
+				timePoint.forecast.push(point)
+			} else {
+				forecastByTime.push({
+					time: point.time,
+					forecast: [point]
+				})
+			}
+		}
+		return forecastByTime
+		
+	}
 }
